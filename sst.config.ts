5004058003005257218c68418af575031414f901
Karel <nagelkarel@gmail.com>
2024-04-01 14:54:08 +0300

    maybe deploys

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+const copyFiles = [{ from: "node_modules/@libsql", to: "node_modules/@libsql" }];
+
 export default $config({
   app: () => ({
     name: "wolf-project",
@@ -10,13 +12,16 @@ export default $config({
   run: async () => {
     const isMain = $app.stage === "main";
     const environment = {
-      // ADMIN_AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
-      // ADMIN_AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+      ADMIN_AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+      ADMIN_AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+      ASTRO_STUDIO_APP_TOKEN: process.env.ASTRO_STUDIO_APP_TOKEN!,
     };
 
     new sst.aws.Astro("Client", {
       path: "packages/client",
       environment,
+      transform: { server: { copyFiles } },
+
       // domain: !isMain
       //   ? undefined
       //   : {
