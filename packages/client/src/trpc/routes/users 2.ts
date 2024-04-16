77a7f6dfb54d5434adb7988c0bfe367815735a87
lovisaviigisalu <122033147+lovisaviigisalu@users.noreply.github.com>
2024-04-16 14:57:05 +0300

    ayrtoni muudatused??

---
+++ packages/client/src/trpc/routes/users 2.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+import { publicProcedure, root } from "../root";
+import { Users, db, like } from "astro:db";
+import { getRandomId } from "@wolf-project/shared/helpers";
+
+const UserZod = z.object({
+  userId: z.string(),
+  name: z.string(),
+  email: z.string(),
+  role: z.string(),
+  language: z.string(),
+  job: z.string().nullable(),
+});
+
+const Client = UserZod.omit({ userId: true, job: true });
+const Employee = UserZod.omit({ userId: true });
+
+export type Client = z.infer<typeof Client>;
+export type Employee = z.infer<typeof Employee>;
+
+export const employee = root.router({
+  create: publicProcedure
+    .input(Employee)
+    .output(UserZod)
+    .mutation(async ({ input: { name, email, role, language, job } }) => {
+      const result = await db
+        .insert(Users)
+        .values({ userId: getRandomId(), name, email, role, language, job })
+        .returning();
+      return result[0]!;
+    }),
+  modify: publicProcedure
+    .input(UserZod)
+    .output(UserZod)
+    .mutation(async ({ input: { userId, name, email, role, language, job } }) => {
+      const result = await db.update(Users)
+        .set({ name, email, role, language, job })
+        .where(like(Users.userId, userId))
+        .returning();
+      return result[0]!;
+    })
+});
+
+export const client = root.router({
+  create: publicProcedure
+    .input(Client)
+    .output(UserZod)
+    .mutation(async ({ input: { name, email, role, language } }) => {
+      const result = await db
+        .insert(Users)
+        .values({ userId: getRandomId(), name, email, role, language })
+        .returning();
+      return result[0]!;
+    }),
+});
